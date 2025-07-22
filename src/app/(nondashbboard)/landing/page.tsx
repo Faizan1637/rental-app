16e660d8855706ed69689797b0c0d4d8889d75c9
Faizan1637 <faizanakram1637@gmail.com>
2025-07-22 23:23:26 +0500

    progresses

--- src/app/(nondashbboard)/landing/page.tsx
+++ src/app/(nondashbboard)/landing/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import HeroSection from './HeroSection'
+import FooterSection from './FooterSection'
+import DiscoverSection from './DiscoverSection'
 function page() {
   return (
     <div>
+      <DiscoverSection/>
       <HeroSection/>
+      <FooterSection/>
     </div>
   )
 }
